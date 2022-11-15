301c0b638f6c4cb3dda9a6656dc727be62d5d98e
506-4 <506-4@BELDORNII.LOCAL>
2022-11-15 08:53:14 +0300

    task3

--- script.js
+++ script.js
@@ -92,4 +92,18 @@
   // Only change code below this line
 
 //------------------------------
+
+
+
+//task 3__________________________________
+
+// function checkObj(obj, checkProp) {
+//   // Only change code below this line
+//   if(obj.hasOwnProperty(checkProp)){
+//     return obj[checkProp];
+//   }else{
+//     return "Not Found";}
+  
+//   // Only change code above this line
+// }
   
