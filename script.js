9e6bbdc4647c272476323f5d09c9ac5d00fda394
506-4 <506-4@BELDORNII.LOCAL>
2022-11-15 08:58:27 +0300

    task3

--- script.js
+++ script.js
@@ -107,3 +107,28 @@
 //   // Only change code above this line
 // }
   
+
+//--------------------------------------
+
+// const myMusic = [
+//   {
+//     "artist": "Billy Joel",
+//     "title": "Piano Man",
+//     "release_year": 1973,
+//     "formats": [
+//       "CS",
+//       "8T",
+//       "LP" ],
+//     "gold": true
+//   },
+//   // Add record here
+//   {
+//     "artist": "Bob",
+//     "title": "Hello",
+//     "release_year": 1950,
+//     "formats": [
+//       "CS",
+//       "8T"
+//     ]
+//   }
+// ];
